8cd2e551455eebe44a99f88ebe58f36703fdbf76
Wit Jakuczun <wit.jakuczun@gmail.com>
2022-03-30 21:47:30 +0200

    Created build task to compile notebook and slides

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
-julia --project=. --eval="using Literate; Literate.notebook(\"test.jl\", \".\"; mdstrings=true)"
-/Users/witj/.julia/conda/3/bin/jupyter nbconvert --to=slides test.ipynb
+SRC_JULIA_FILE=$1.jl
+OUT_IPYNB_FILE=$1.ipynb
+julia --project=. --eval="using Literate; Literate.notebook(\"$SRC_JULIA_FILE\", \".\"; mdstrings=true)"
+/Users/witj/.julia/conda/3/bin/jupyter nbconvert --to=slides $OUT_IPYNB_FILE
